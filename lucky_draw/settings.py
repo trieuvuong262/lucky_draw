c10bdfa56cf8455d5d1f2076d0ecf8e045def91c
Le Nguyen Trieu Vuong <ltv002-bp@fhmc.com>
2026-01-19 14:08:47 +0700

    Update allowed hosts

--- lucky_draw/settings.py
+++ lucky_draw/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-sg%(5*xg=wim@h+_u$cc9v@nvs+yx!jyrd0ljkzi#@4i%u8fht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['trieuvuong1997.pythonanywhere.com']
+ALLOWED_HOSTS = ['trieuvuong1997.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
